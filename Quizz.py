a5042567905be4cfce76b72e6211e1cb22e606eb
juliapinheiro2009jj-beep <juliapinheiro2009jj@gmail.com>
2026-05-08 19:32:35 -0300

    Quizz.py
    Added a history quiz with three questions and answers.

--- Quizz.py
+++ Quizz.py
@@ -1,1 +1,31 @@
-
+# prova de história 
+print("1. Em que ano o Brasil declarou sua Independência de Portugal?")
+ print("A) 1500")
+ print("B) 1822")
+ print("C) 1889")
+ resposta1 = input("Sua resposta: ").upper()
+ if resposta1 == "B":
+     print("✅ Você ACERTOU! A Independência foi em 1822.\n")
+ else:
+     print("❌ Você ERROU! A resposta correta é B) 1822\n")
+ # PERGUNTA 2
+ print("2. Qual povo habitava o Brasil antes da chegada dos portugueses?")
+ print("A) Gregos e Romanos")
+ print("B) Índios e povos nativos")
+ print("C) Egípcios")
+ resposta2 = input("Sua resposta: ").upper()
+ if resposta2 == "B":
+     print("✅ Você ACERTOU! Os povos indígenas já viviam aqui há milhares de anos.\n")
+ else:
+     print("❌ Você ERROU! A resposta correta é B) Índios e povos nativos\n")
+ # PERGUNTA 3
+ print("3. O que foi o Ciclo do Ouro no Brasil Colonial?")
+ print("A) Período de exploração de ouro e pedras preciosas")
+ print("B) Época da chegada da Família Real")
+ print("C) Período da proclamação da República")
+ resposta3 = input("Sua resposta: ").upper()
+ if resposta3 == "A":
+     print("✅ Você ACERTOU! Foi o momento onde a mineração foi a principal atividade econômica.")
+ else:
+     print("❌ Você ERROU! A resposta correta é A) Período de exploração de ouro e pedras preciosas")
+ criado por Julia Pinheiro da Silva 
